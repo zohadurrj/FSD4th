e236c7abf39e3bc827e8db5cd1f452e9b05bdcb0
zohadurrj <zohadur.23b0131212@abes.ac.in>
2025-04-16 12:02:33 +0530

    React

---
+++ REACT/First/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-3xl text-red-700 font-bold underline'>
+      This is me Zohadur Starting With React
+    </div>
+  )
+}
+
+export default App
